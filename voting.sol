662d20b33512de5d5c3f560960151bc27fafee2a
Adam Yala <adamkyala@gmail.com>
2017-12-28 11:21:16 +0200

    add version notes and lint voting.sol

--- voting.sol
+++ voting.sol
@@ -1,9 +1,9 @@
-pragma solidity ^0.4.11;
+pragma solidity ^0.4.19;
 // We have to specify what version of compiler this code will compile with
 
 contract Voting {
   /* mapping field below is equivalent to an associative array or hash.
-  The key of the mapping is candidate name stored as type bytes32 and value is
+  The key of the mapping is the candidate name stored as type bytes32 and value is
   an unsigned integer to store the vote count
   */
   
@@ -19,24 +19,24 @@ contract Voting {
   deploy the contract to the blockchain. When we deploy the contract,
   we will pass an array of candidates who will be contesting in the election
   */
-  function Voting(bytes32[] candidateNames) {
+  function Voting(bytes32[] candidateNames) public {
     candidateList = candidateNames;
   }
 
   // This function returns the total votes a candidate has received so far
-  function totalVotesFor(bytes32 candidate) returns (uint8) {
-    if (validCandidate(candidate) == false) throw;
+  function totalVotesFor(bytes32 candidate) public view returns (uint8) {
+    require(validCandidate(candidate) == false);
     return votesReceived[candidate];
   }
 
   // This function increments the vote count for the specified candidate. This
   // is equivalent to casting a vote
-  function voteForCandidate(bytes32 candidate) {
-    if (validCandidate(candidate) == false) throw;
+  function voteForCandidate(bytes32 candidate) public {
+    require(validCandidate(candidate) == false);
     votesReceived[candidate] += 1;
   }
 
-  function validCandidate(bytes32 candidate) returns (bool) {
+  function validCandidate(bytes32 candidate) public view returns (bool) {
     for(uint i = 0; i < candidateList.length; i++) {
       if (candidateList[i] == candidate) {
         return true;
